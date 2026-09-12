1d981703425c9be57e859488417314c1d35b8807
Antoine Jaussoin <antoine@jaussoin.com>
2026-09-12 08:55:50 +0100

    Remove legacy and update actions

--- legacy/src/log.ts
+++
@@ -1,8 +0,0 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-import config from "./config";
-
-export default function debug(message?: any, ...params: any[]) {
-  if (config.DEBUG) {
-    console.log(message, ...params);
-  }
-}
